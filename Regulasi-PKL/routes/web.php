9394f2cbe61367a0df7eb3661c6d7b61bac62fa3
delia041015 <delialistiana025@gmail.com>
2023-11-07 19:33:44 +0700

    2

--- Regulasi-PKL/routes/web.php
+++ Regulasi-PKL/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+use App\Http\Controllers\MahasiswaController;
+
+Route::get('tampil-mhs', [MahasiswaController::class, 'index']);
+
