d0124f86a71077913b5278aadb64e0234b9199c6
putriam1 <148181244+putriam1@users.noreply.github.com>
2023-11-07 14:19:27 +0700

    1

--- Regulasi-PKL/resources/views/mahasiswa/index.blade.php
+++ Regulasi-PKL/resources/views/mahasiswa/index.blade.php
@@ -1,28 +1,26 @@
 <div class="container">
     <h2>Tabel Mahasiswa</h2>
-    <a href="{{route('produk.create')}}" class="btn btn-success">+New Data</a>
-    <table class="table table-bordered table striped" id="tabel-produk">
+    <a href="{{route('mahasiswa.create')}}" class="btn btn-success">+New Data</a>
+    <table class="table table-bordered table striped" id="tabel-mahasiswa">
         <thead>
             <tr>
                 <th style="width:1%">No.</th>
-                <th style="width:5%">Kode Produk</th>
-                <th style="width:5%">Nama Produk</th>
-                <th style="width:5%">Harga</th>
-                <th style="width:5%">Stok</th>
+                <th style="width:5%">NIM</th>
+                <th style="width:5%">Nama Mahasiswa</th>
+                <th style="width:5%">Kelas</th>
                 <th style="width:5%">Aksi</th>
             </tr> 
         </thead>
         <tbody>
-            @foreach ($dataProduk as $data) 
+            @foreach ($dataMahasiswa as $data) 
             <tr>
                 <td> {{ $loop->iteration }}</td>
-                <td> {{ $data->id }}</td>
-                <td> {{ $data->nama_produk }}</td>
-                <td> {{ number_format($data->harga, 0, ',' , '.') }}</td>
-                <td> {{ $data->stock }}</td> 
+                <td> {{ $data->nim }}</td>
+                <td> {{ $data->nama }}</td>
+                <td> {{ $data->kelas }}</td> 
                 <td> 
-                    <form action="{{route('produk.delete', $data->id)}}" method="post">@csrf
-                        <a href="{{route('produk.edit', $data->id)}}" class="btn btn-warning">Edit</a>
+                    <form action="{{route('mahasiswa.delete', $data->nim)}}" method="post">@csrf
+                        <a href="{{route('mahasiswa.edit', $data->nim)}}" class="btn btn-warning">Edit</a>
                         <button class = "btn btn-danger">Delete</button>
                     </form>
                 </td>
