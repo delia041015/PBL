0ed90ecf9b98182b4bad4d805e17bbb34cc11704
putriam1 <148181244+putriam1@users.noreply.github.com>
2023-11-07 20:02:03 +0700

    1

--- Regulasi-PKL/routes/web.php
+++ Regulasi-PKL/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\MahasiswaController;
+Route::get('tampil-mahasiswa', [MahasiswaController::class, 'index']);
