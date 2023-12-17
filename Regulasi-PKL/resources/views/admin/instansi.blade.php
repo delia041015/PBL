f7c4cb9f7e54186aaac4966138ed32c48c2440b9
delia041015 <delialistiana025@gmail.com>
2023-12-17 20:41:18 +0700

    flasshhh ins

--- Regulasi-PKL/resources/views/admin/instansi.blade.php
+++ Regulasi-PKL/resources/views/admin/instansi.blade.php
@@ -94,15 +94,7 @@
 				</button>
 			</div>
 
-      @if ($errors->any())
-      <div class="alert alert-danger">
-          <ul>
-              @foreach ($errors->all() as $error)
-              <li>{{$error}}</li>
-              @endforeach
-          </ul>
-      </div>
-      @endif
+     
       
 
 			<div class="modal-body">
