896883825f399f71bbed632600c7a070809d5289
putriam1 <148181244+putriam1@users.noreply.github.com>
2023-12-18 21:06:41 +0700

    usercreate

--- Regulasi-PKL/app/Models/User.php
+++ Regulasi-PKL/app/Models/User.php
@@ -17,7 +17,11 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+    
+    protected $table = 'users';
+    protected $primaryKey = 'id';
     protected $fillable = [
+        'id',
         'name',
         'email',
         'password',
