288dae658c4e9c37619197273991144b78ed6440
delia041015 <delialistiana025@gmail.com>
2023-12-19 12:16:38 +0700

    lagi

--- Regulasi-PKL/resources/views/layouts/sidebar_mahasiswa.blade.php
+++ Regulasi-PKL/resources/views/layouts/sidebar_mahasiswa.blade.php
@@ -923,8 +923,8 @@
     $('#tabel-kelompok').DataTable({paging: true,  pageLength: 5});
     $('#tabel-instansi').DataTable({paging: true,  pageLength: 5});
     $('#tabel-surat').DataTable({paging: true,  pageLength: 5});
-    $('#tabel-mahasiswa').DataTable(),
-    $('#tabel-kelompok').DataTable();
+    $('#tabel-mahasiswa').DataTable({paging: true,  pageLength: 5}),
+    $('#tabel-kelompok').DataTable({paging: true,  pageLength: 5});
     });
 </script>
 </html>
